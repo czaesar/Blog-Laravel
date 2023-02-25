0f64a66d533cabbd67416cfaa53c44f84f5166dd
Valya <unsterdam@mail.ru>
2023-02-25 10:15:31 +0300

    Update post

--- resources/views/admin/post/edit.blade.php
+++ resources/views/admin/post/edit.blade.php
@@ -28,12 +28,12 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class ="col-12">
-                    <form action="{{route('admin.post.update', $post->id)}}" method="POST"  class="w-50 ">
+                    <form action="{{route('admin.post.update', $post->id)}}" method="POST" enctype="multipart/form-data"  class="w-50 ">
                         @csrf <!--если метод пост, нужен csrf иначе нихуя не заработает -->
                         @method('PATCH')
                         <div class="form-group w-25" >
                             <input type="text" class="form-control" name="title"  placeholder="Название постов"
-                                   value="{{$post->title }}"
+                                   value="{{ $post->title }}"
                             >
                             @error('title')
                             <div class="text-danger">Это поле необходимо для заполнения</div>
@@ -41,7 +41,7 @@
                         </div>
                         <div class="form-group">
                             <textarea id="summernote" name="content">
-                                {{$post->content}}
+                                {{ $post->content }}
                             </textarea>
                             @error('title')
                             <div class="text-danger">Это поле необходимо для заполнения</div>
@@ -49,8 +49,8 @@
                         </div>
                         <div class="form-group w-50">
                             <label for="exampleInputFile">Добавить превью</label>
-                            <div class="w-25">
-                                <img src="{{public_path($post->preview_image)}}" alt="preview_image" class ="w-50">
+                            <div class="w-75 mb-2">
+                                <img src="{{ url('storage/' . $post->preview_image) }}" alt="preview_image" class ="w-50">
                             </div>
                             <div class="input-group">
                                 <div class="custom-file">
@@ -67,8 +67,8 @@
                         </div>
                         <div class="form-group w-50">
                             <label for="exampleInputFile">Добавить главное изображение</label>
-                            <div class="w-50">
-                                <img src="{{url('storage/' . $post->main_image)}}" alt="main_image" class ="w-50">
+                            <div class="w-100 mb-2">
+                                <img src="{{ url('storage/' . $post->main_image) }}" alt="main_image" class ="w-50">
                             </div>
                             <div class="input-group">
                                 <div class="custom-file">
@@ -88,8 +88,8 @@
                             <select name="category_id" class="form-control">
                                 @foreach($categories as $category)
                                     <option value="{{$category->id}}"
-                                        {{$category->id == $post->category_id? 'selected': ""}}
-                                    > {{$category->title}}</option>
+                                        {{ $category->id == $post->category_id ? 'selected': "" }}
+                                    > {{ $category->title }}</option>
                                 @endforeach
 
                             </select>
@@ -98,7 +98,7 @@
                             <label>Тэги</label>
                             <select class="select2" name ="tag_ids[]" multiple="multiple" data-placeholder="Выберите теги" style="width: 100%;">
                                 @foreach($tags as $tag)
-                                    <option {{is_array($post->tags->pluck('id')) && in_array( $tag->id, $post->tags->pluck('id')) ? 'selected': "" }} value="{{$tag->id}}">{{$tag->title}}</option>
+                                    <option {{ is_array($post->tags->pluck('id')->toArray() ) && in_array( $tag->id, $post->tags->pluck('id')->toArray() ) ? 'selected': "" }} value="{{ $tag->id }}">{{ $tag->title }}</option>
 
                                 @endforeach
                             </select>
