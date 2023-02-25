8f68f7cae79313dd2ab8462e354c1fc8f0fde722
Valya <unsterdam@mail.ru>
2023-02-25 11:04:35 +0300

    Soft_delete_post(trouble with SQLITE)

--- database/migrations/2023_02_25_071428_add_soft_delete_to_posts_table.php
+++ database/migrations/2023_02_25_071428_add_soft_delete_to_posts_table.php
@@ -14,7 +14,7 @@ class AddSoftDeleteToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->softDeletes();
         });
     }
 
@@ -26,7 +26,7 @@ class AddSoftDeleteToPostsTable extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            //
+            $table->dropSoftDeletes();
         });
     }
 }
